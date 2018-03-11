fc8fb41c18992bfaace5621754916fc057f7eebf
pawan1773 <pawan1773>
2018-03-11 11:22:21 -0700

    add message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
-const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const http = require('http');
 
-const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server running on port ${port}.`));
+io.on('connection', (socket) => {
+    console.log('User connected');
 
+    socket.emit('newMessage', {
+        from: 'Manmeet',
+        text: 'see you then',
+        createdAt: new Date()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}.`));
